79d9cf0d8e5653a531a330fbcf20d6eced26d4a2
yurisantos-y <yuri01.sp@gmail.com>
2024-05-01 16:43:15 -0300

    Subindo todos os aplicativos

---
+++ primeiro_projeto/lib/main.dart
@@ -0,0 +1,81 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Frases do dia'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  int _numeroAleatorio = 0;
+  final List _frases = [
+    'Quem não pode errar sou eu.',
+    'Sendo melhor que ontem e pior que amanhã.',
+    'Você é o resumo dos seus três amigos mais próximos.',
+    'Forjado para coisas grandes'
+  ];
+
+  void _incrementCounter() {
+    setState(() {
+
+      _numeroAleatorio = Random().nextInt(4);
+
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Pressione o botão para gerar mais uma frase:',
+            ),
+            Text(
+              _frases[_numeroAleatorio],
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
