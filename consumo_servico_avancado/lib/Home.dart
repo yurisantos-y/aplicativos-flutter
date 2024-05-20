23d1dad6d0e1d309085ec42da25b56246ce7b01e
yurisantos-y <yuri01.sp@gmail.com>
2024-05-20 14:39:14 -0300

    Criando requisicao com Post

--- consumo_servico_avancado/lib/Home.dart
+++ consumo_servico_avancado/lib/Home.dart
@@ -42,40 +42,91 @@ class _HomeState extends State<Home> {
     }
   }
 
+  _post() async {
+    var corpo = json.encode({
+      "userID": 120,
+      "id": null, // Usually servers generate IDs automatically
+      "title": "Titulo",
+      "body": "Corpo da postagem"
+    });
+
+    http.Response response = await http.post(
+      Uri.parse(_urlBase + "/posts"),
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8"
+      },
+      body: corpo,
+    );
+
+    print("resposta: ${response.statusCode}");
+    print("resposta: ${response.body}");
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Consumo de serviço avançado"),
       ),
-      body: FutureBuilder<List<Post>>(
-        future: _recuperarPostagens(),
-        builder: (context, snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-            case ConnectionState.waiting:
-              return Center(child: CircularProgressIndicator());
-            case ConnectionState.active:
-            case ConnectionState.done:
-              if (snapshot.hasError) {
-                return Text("Erro ao carregar posts");
-              } else {
-                return ListView.builder(
-                  itemCount: snapshot.data?.length ?? 0,
-                  itemBuilder: (context, index) {
-                    if (snapshot.data == null) return Container();
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                ElevatedButton(
+                    onPressed: (){
+                      _post();
+                    },
+                    child: Text("Salvar")
+                ),
+                ElevatedButton(
+                    onPressed: (){
+                      _post();
+                    },
+                    child: Text("Atualizar")
+                ),
+                ElevatedButton(
+                    onPressed: (){
+                      _post();
+                    },
+                    child: Text("Remover")
+                ),
+              ],
+            ),
+            Expanded(
+                child: FutureBuilder<List<Post>>(
+                  future: _recuperarPostagens(),
+                  builder: (context, snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.none:
+                      case ConnectionState.waiting:
+                        return Center(child: CircularProgressIndicator());
+                      case ConnectionState.active:
+                      case ConnectionState.done:
+                        if (snapshot.hasError) {
+                          return Text("Erro ao carregar posts");
+                        } else {
+                          return ListView.builder(
+                            itemCount: snapshot.data?.length ?? 0,
+                            itemBuilder: (context, index) {
+                              if (snapshot.data == null) return Container();
 
-                    final Post post = snapshot.data![index];
+                              final Post post = snapshot.data![index];
 
-                    return ListTile(
-                      title: Text(post.title),
-                      subtitle: Text(post.id.toString()),
-                    );
+                              return ListTile(
+                                title: Text(post.title),
+                                subtitle: Text(post.id.toString()),
+                              );
+                            },
+                          );
+                        }
+                    }
                   },
-                );
-              }
-          }
-        },
+            ))
+          ],
+        ),
       ),
     );
   }
